350f4ffb130e0422971f2c6407d4b11562ba4ad3
Laura Docherty <laurahdocherty@gmail.com>
2020-07-20 14:58:32 -0500

    Static method built

--- desserts.py
+++ desserts.py
@@ -11,24 +11,38 @@ class Cupcake:
       self.flavor = flavor
       self.price = price
       self.qty = 0
+      self.cache[self.name] = self
 
     def add_stock(self, amount):
 
-      return self.qty + amount
+      self.qty += amount
 
     def sell(self, amount):
 
-      if add_stock() == 0:
+      if self.qty == 0:
         print('Sorry, these cupcakes are sold out')
-      
+
+      if amount > self.qty:
+        self.qty = 0
+
+      else:
+        self.qty = self.qty - amount
 
     def __repr__(self):
         """Human-readable printout for debugging."""
 
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
 
+    @staticmethod
+    def scale_recipe(ingredients, amount):
 
+      ingredients_scaled = []
 
+      for ingredient in ingredients:
+        ingredient_tuple = (ingredient[0], (ingredient[1] * amount))
+        ingredients_scaled.append(ingredient_tuple)
+
+      return ingredients_scaled
 
 if __name__ == '__main__':
     import doctest
